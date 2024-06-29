740191577fe8f94c5e3341c9545adef919fdcd06
Hammadsheikh5 <mhammadkhi5@gmail.com>
2024-06-29 16:01:54 +0500

    Creating project3

---
+++ Typescript_P3_ATM/index.js
@@ -0,0 +1,4 @@
+#! /usr/bin/env node
+const myAccountBalance = 15000;
+const myAccountPin = 8330;
+export {};
---
+++ Typescript_P3_ATM/index.ts
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const myAccountBalance :number = 15000
+const myAccountPin :number = 8330
+
