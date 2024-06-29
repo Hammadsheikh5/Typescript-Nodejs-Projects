6479e0b038f06eb849969c5ae9224d38ccce6890
Hammadsheikh5 <mhammadkhi5@gmail.com>
2024-06-29 17:32:59 +0500

    Formatting

--- Typescript_P3_ATM/index.ts
+++ Typescript_P3_ATM/index.ts
@@ -16,89 +16,90 @@ const userAuthentication = await inquirer.prompt([
     },
 ]);
 if (userAuthentication.userPin === myAccountPin) {
- console.log(chalk.green.bold("\nCorrect Pin\nNow you can use our services\n"));
- while(true){
+    console.log(chalk.green.bold("\nCorrect Pin\nNow you can use our services\n"));
+    while (true) {
 
-    const operations = await inquirer.prompt([
-        {
-            name: "operation",
-            type: "list",
-            message: chalk.blue.bold(" What operation do you want to perform ? "),
-            choices: ["Deposit", "Fast Cash", "Withdraw", "Check Balance","Quit"],
-        },
-    ]);
-    if (operations.operation === "Deposit") {
-        const depositAmount = await inquirer.prompt([
+        const operations = await inquirer.prompt([
             {
-                name: "Amount",
-                type: "number",
-                message: chalk.blue.bold(" Enter the amount you want to deposit : "),
+                name: "operation",
+                type: "list",
+                message: chalk.blue.bold(" What operation do you want to perform ? "),
+                choices: ["Deposit", "Fast Cash", "Withdraw", "Check Balance", "Quit"],
             },
         ]);
-        myAccountBalance += depositAmount.Amount;
-        console.log(chalk.green.bold(`\nYour account balance is ${myAccountBalance}\n`));
-    } else if (operations.operation === "Fast Cash") {
-        while (true) {
-            const fastCashAmount = await inquirer.prompt([
+        if (operations.operation === "Deposit") {
+            const depositAmount = await inquirer.prompt([
                 {
-                    name: "yourAmount",
-                    message: chalk.blue.bold("\nSelect any amount\n"),
-                    type: "list",
-                    choices: [
-                        "500",
-                        "1000",
-                        "2000",
-                        "3000",
-                        "4000",
-                        "5000",
-                        "6000",
-                        "10000",
-                        "20000",
-                    ],
-                },
-            ]);
-            if (fastCashAmount.yourAmount > myAccountBalance) {
-                console.log(chalk.red.bold("\nInsufficient Balance , try another amount"));
-                continue;
-            } else {
-                myAccountBalance -= fastCashAmount.yourAmount;
-                console.log(chalk.green.bold(`\nYou withdraw your amount successsfully `));
-                console.log(chalk.green.bold(
-                    `Your remaining account balance is ${myAccountBalance}\n`
-                ));
-                break;
-            }
-        }
-    } else if (operations.operation === "Withdraw") {
-        while (true) {
-            const withdrawAmount = await inquirer.prompt([
-                {
-                    name: "userwithdrawAmount",
-                    message: chalk.blue.bold("\nEnter the amount you want to withdraw in multiples of 500 : "),
+                    name: "Amount",
                     type: "number",
+                    message: chalk.blue.bold(" Enter the amount you want to deposit : "),
                 },
             ]);
-            if (withdrawAmount.userwithdrawAmount > myAccountBalance) {
-                console.log(chalk.red.bold("\nInsufficient Balance , try another amount"));
-                continue;
-            } else {
-                myAccountBalance -= withdrawAmount.userwithdrawAmount;
-                console.log(chalk.green.bold(`\nYou withdraw your amount successsfully `));
-
-                console.log(chalk.green.bold(
-                    `Your remaining account balance is ${myAccountBalance}\n`
-                ));
-                break;
+            myAccountBalance += depositAmount.Amount;
+            console.log(chalk.green.bold(`\nYour account balance is ${myAccountBalance}\n`));
+        } else if (operations.operation === "Fast Cash") {
+            while (true) {
+                const fastCashAmount = await inquirer.prompt([
+                    {
+                        name: "yourAmount",
+                        message: chalk.blue.bold("\nSelect any amount\n"),
+                        type: "list",
+                        choices: [
+                            "500",
+                            "1000",
+                            "2000",
+                            "3000",
+                            "4000",
+                            "5000",
+                            "6000",
+                            "10000",
+                            "20000",
+                        ],
+                    },
+                ]);
+                if (fastCashAmount.yourAmount > myAccountBalance) {
+                    console.log(chalk.red.bold("\nInsufficient Balance , try another amount"));
+                    continue;
+                } else {
+                    myAccountBalance -= fastCashAmount.yourAmount;
+                    console.log(chalk.green.bold(`\nYou withdraw your amount successsfully `));
+                    console.log(chalk.green.bold(
+                        `Your remaining account balance is ${myAccountBalance}\n`
+                    ));
+                    break;
+                }
             }
-        }
-    } else if (operations.operation === "Check Balance") {
-        console.log(chalk.green.bold(`\nYour current balance is ${myAccountBalance}\n`));
-    } else if (operations.operation==="Quit"){
-        console.log(chalk.green.bold("Thank you for choosing our ATM"));
-        break;
-    }
+        } else if (operations.operation === "Withdraw") {
+            while (true) {
+                const withdrawAmount = await inquirer.prompt([
+                    {
+                        name: "userwithdrawAmount",
+                        message: chalk.blue.bold("\nEnter the amount you want to withdraw in multiples of 500 : "),
+                        type: "number",
+                    },
+                ]);
+                if (withdrawAmount.userwithdrawAmount > myAccountBalance) {
+                    console.log(chalk.red.bold("\nInsufficient Balance , try another amount"));
+                    continue;
+                } else {
+                    myAccountBalance -= withdrawAmount.userwithdrawAmount;
+                    console.log(chalk.green.bold(`\nYou withdraw your amount successsfully `));
 
-}}
- else {
+                    console.log(chalk.green.bold(
+                        `Your remaining account balance is ${myAccountBalance}\n`
+                    ));
+                    break;
+                }
+            }
+        } else if (operations.operation === "Check Balance") {
+            console.log(chalk.green.bold(`\nYour current balance is ${myAccountBalance}\n`));
+        } else if (operations.operation === "Quit") {
+            console.log(chalk.green.bold("Thank you for choosing our ATM"));
+            break;
+        }
+
+    }
+}
+else {
     console.log(chalk.red.bold("\nInvalid Pin code ! \nPlease try again."));
 }
